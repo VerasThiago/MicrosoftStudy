0675952e3b985d14f9fda3419873a220c869a94a
VerasThiago <thiago.veras.machado@gmail.com>
2020-07-16 23:52:07 -0300

    Add days count

--- functions.js
+++ functions.js
@@ -3,9 +3,19 @@ function setValue(id){
     var qnt = 0;
     for(var i = 0; i < elms.length; i++) 
       qnt += elms[i].checked 
-    document.getElementById("total" + id ).innerHTML = qnt + '/' + elms.length
+    document.getElementById("Total" + id ).innerHTML = qnt + '/' + elms.length
+}
+
+function setDate(){
+    var d = new Date();
+    const oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
+    const firstDate = new Date(2020, 6, 13);
+    const secondDate = new Date(d.getFullYear(), d.getMonth(), d.getDate());
+    const diffDays = Math.round(Math.abs((firstDate - secondDate) / oneDay)) ;
+    document.getElementById("Days").innerHTML = 'Total days: ' + (diffDays + 1) ;
 }
 
 function getQuantity(){
     setValue("med")
+    setDate()
 }
