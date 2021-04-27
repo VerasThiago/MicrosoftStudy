487264ba9aaef0490be40ad128460ccbe249ab1f
VerasThiago <thiago.veras.machado@gmail.com>
2021-04-27 00:31:17 -0300

    Third edition

---
+++ functions3.js
@@ -0,0 +1,20 @@
+function setValue(id){
+    var elms = document.querySelectorAll("[id='" + id + "']");
+    var qnt = 0;
+    for(var i = 0; i < elms.length; i++) 
+      qnt += elms[i].checked 
+    document.getElementById("Total" + id ).innerHTML = qnt + '/' + elms.length
+}
+
+function setDate(){
+    const date1 = new Date('4/27/2021');
+    const date2 = new Date();
+    const diffTime = Math.abs(date2 - date1);
+    const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24)); 
+    document.getElementById("Days").innerHTML = 'Total days: ' + diffDays;
+}
+
+function getQuantity(){
+    setValue("med")
+    setDate()
+}
